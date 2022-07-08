314d43c2eb088d322aac0932bcd9a1b3a6ee15b4
Alexander Hörl <alexander.horl2@live.de>
2022-07-08 08:54:38 +0200

    changed api endpoint on production

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export const apiEndpoint =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.nureinberg.de'
+    ? 'https://nureinberg-bestand-api.alexanderhoerl.de'
     : 'http://localhost:3000';
